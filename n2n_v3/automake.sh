7b57fe63dc14bbf36259f3c34521740c3d21adf8
yyqt <you@vip.ever2010.com>
2022-05-01 11:42:54 +0800

    automake

--- n2n_v3/automake.sh
+++ n2n_v3/automake.sh
@@ -11,7 +11,7 @@ NAME=linux_aarch64_10
 mkdir -p ./outs/$NAME
 cp ./Makefile ./Makefile_tmp
 make clean
-sed 's/CC=gcc/CC=\/home\/yyqt\/n2n\/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu\/bin\/aarch64-none-linux-gnu-gcc/g' /Makefile_tmp
+sed 's/CC=gcc/CC=\/home\/yyqt\/n2n\/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu\/bin\/aarch64-none-linux-gnu-gcc/g' ./Makefile_tmp
 make -f ./Makefile_tmp
 cp ./edge ./outs/$NAME/edge
 cp ./supernode ./outs/$NAME/supernode
@@ -21,7 +21,7 @@ NAME=linux_aarch64_8
 mkdir -p ./outs/$NAME
 cp ./Makefile ./Makefile_tmp
 make clean
-sed 's/CC=gcc/CC=\/home\/yyqt\/n2n\/gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu\/bin\/aarch64-none-linux-gnu-gcc/g' /Makefile_tmp
+sed 's/CC=gcc/CC=\/home\/yyqt\/n2n\/gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu\/bin\/aarch64-none-linux-gnu-gcc/g' ./Makefile_tmp
 make -f ./Makefile_tmp
 cp ./edge ./outs/$NAME/edge
 cp ./supernode ./outs/$NAME/supernode
