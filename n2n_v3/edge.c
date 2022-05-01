f2c1bbb38e5f0bbac3ebd66b5bde0ef3b0f7515d
yyqt <yyqt@vip.ever2010.com>
2022-05-01 09:07:24 +0800

    defaultthread1

--- n2n_v3/edge.c
+++ n2n_v3/edge.c
@@ -1304,7 +1304,7 @@ int main(int argc, char* argv[]) {
   int     i, effectiveargc=0;
   char ** effectiveargv=NULL;
   char  * linebuffer = NULL;
-  int threadcount = 2; //默认线程数
+  int threadcount = 1; //默认线程数
   n2n_edge_t eee; /* single instance for this program */
 
   if (-1 == edge_init(&eee) ){
